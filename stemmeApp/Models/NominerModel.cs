0fe676e0f6ac8cd7a375bb9e5792c366fad956b4
AleksanderNilsson <71332420+AleksanderNilsson@users.noreply.github.com>
2021-04-20 13:31:31 +0200

    Add project files.

---
+++ stemmeApp/Models/NominerModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace stemmeApp.Models
+{
+    public class NominerModel
+    {
+        [DataType(DataType.EmailAddress)]
+        public string Epost { get; set; }
+        [Required(ErrorMessage = "Kandidaten må ha ett fornavn")]
+        public string Fornavn { get; set; }
+        [Required(ErrorMessage = "Kandidaten må ha ett etternavn")]
+        public string Etternavn { get; set; }
+        [Display(Name = "Skriv litt om kandidaten")]
+        [Required(ErrorMessage = "Du må skrive litt om kandidaten")]
+        public string info { get; set; }
+        [DataType(DataType.Upload)]
+        public long bilde { get; set; }
+
+
+    }
+}
