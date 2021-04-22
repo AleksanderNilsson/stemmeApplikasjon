357b7325ab7fbd6d7a191393e4d93e9caaa47154
AleksanderNilsson <71332420+AleksanderNilsson@users.noreply.github.com>
2021-04-22 13:38:39 +0200

    CREATE TABLE setninger

--- AspNet.Identity.MySQL/MySQLIdentity.sql
+++ AspNet.Identity.MySQL/MySQLIdentity.sql
@@ -50,3 +50,43 @@ CREATE TABLE `userroles` (
   CONSTRAINT `ApplicationUser_Roles` FOREIGN KEY (`UserId`) REFERENCES `users` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   CONSTRAINT `IdentityRole_Users` FOREIGN KEY (`RoleId`) REFERENCES `roles` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ;
+
+CREATE TABLE candidate(
+Id VARCHAR(256),
+UserName VARCHAR(256) NOT NULL,
+Faculty VARCHAR(100),
+Institute VARCHAR(100),
+Info LONGTEXT,
+Picture INT UNIQUE,
+Primary key (Id),
+foreign key (UserName) references users(UserName)
+);
+
+
+CREATE TABLE votes(
+Voter VARCHAR(256),
+Votedon VARCHAR(256),
+primary key (Voter),
+foreign key (Voter) references users(UserName),
+foreign key (Votedon) references Candidate(Username)
+);
+
+CREATE TABLE election(
+Idelection INT,
+Startelection DATETIME,
+Endelection DATETIME,
+Controlled DATETIME,
+Title VARCHAR(100),
+primary key (Idelection)
+);
+
+CREATE TABLE picture(
+Idpicture INT,
+Loc VARCHAR(100),
+Text VARCHAR(256),
+Alt VARCHAR(100),
+primary key(Idpicture),
+foreign key (Idpicture) references candidate(Picture)
+);
+
+
