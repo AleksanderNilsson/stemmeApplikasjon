d6de66528f14c7c316a426c4cdfbab8a8b813971
AleksanderNilsson <71332420+AleksanderNilsson@users.noreply.github.com>
2021-06-04 11:32:49 +0200

    kommentert kode

--- stemmeApp/Models/ResultsModel.cs
+++ stemmeApp/Models/ResultsModel.cs
@@ -5,12 +5,12 @@ using System.Web;
 
 namespace stemmeApp.Models
 {
-    
+    // SKREVET AV ALEKSANDER NILSOON : Sist endret 04.06.2021 
 
-        public class ResultsViewModel
-        {
-            public List<CandidateVotes> CandidateVotes { get; set; } = new List<CandidateVotes>();
-            public List<ElectionInformation> ElectionInformation { get; set; } = new List<ElectionInformation>();
-        }
+    public class ResultsViewModel
+    {
+        public List<CandidateVotes> CandidateVotes { get; set; } = new List<CandidateVotes>();
+        public List<ElectionInformation> ElectionInformation { get; set; } = new List<ElectionInformation>();
+    }
     
 }
